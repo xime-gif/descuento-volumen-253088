75819e15d1727fde3bdb66a64def874ab292eefd
xime-gif <rosalesximena2813@gmail.com>
2026-09-22 21:30:46 -0700

    Update DescuentoFronteraTest.java

--- descuento-volumen/src/test/java/mx/itson/devops/descuento/DescuentoFronteraTest.java
+++ descuento-volumen/src/test/java/mx/itson/devops/descuento/DescuentoFronteraTest.java
@@ -1,8 +1,6 @@
 package mx.itson.devops.descuento;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 
 /**
